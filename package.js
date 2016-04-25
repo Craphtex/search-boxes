4c5c873da870b7291571c4b95c523a7dc503c829
Christian Lindgren <christian@crapht.se>
2016-04-25 15:20:36 +0900

    Added html file as file instead and use template

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'craphtex:search-boxes',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'Easyily add search boxes to your meteor project',
   git: 'https://github.com/Craphtex/search-boxes.git',
   documentation: 'README.md'
@@ -8,8 +8,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.3.2');
-  api.use('ecmascript');
-  api.use('space:template-controller@0.2.1');
-  api.addAssets('search_boxes.html', 'client');
+  api.use(['templating', 'ecmascript', 'space:template-controller@0.2.1']);
+  api.addFiles('search_boxes.html', 'client');
   api.mainModule('search_boxes.js', 'client');
 });

324e3a90c5113df9fc71251b02b583f06958723a
Christian Lindgren <christian@crapht.se>
2016-04-25 14:58:31 +0900

    Fixed bug regarding Template Controller only working on client side

--- package.js
+++ package.js
@@ -1,14 +1,15 @@
 Package.describe({
   name: 'craphtex:search-boxes',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Easyily add search boxes to your meteor project',
   git: 'https://github.com/Craphtex/search-boxes.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3.2.4');
+  api.versionsFrom('1.3.2');
   api.use('ecmascript');
   api.use('space:template-controller@0.2.1');
-  api.mainModule('search-boxes.js');
+  api.mainModule('search_boxes.html', 'client');
+  api.mainModule('search_boxes.js', 'client');
 });
